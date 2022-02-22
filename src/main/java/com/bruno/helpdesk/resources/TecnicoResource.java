0f53a9cdea7ba65c2c8fb17e7d26d1ac274a5d86
BrunoN97 <nobre.piaget@gmail.com>
2022-02-22 20:30:56 -0300

    findAll para Tecnico, quando pesquisa no postman traz todos os tecnicos

--- src/main/java/com/bruno/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/bruno/helpdesk/resources/TecnicoResource.java
@@ -1,7 +1,11 @@
 package com.bruno.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +26,12 @@ public class TecnicoResource {
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }

e343e615033b97387cc9de4cc54a4b9b13f3090a
BrunoN97 <nobre.piaget@gmail.com>
2022-03-07 19:28:16 -0300

    Validação de CPF e email ja cadastrados

--- src/main/java/com/bruno/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/bruno/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.bruno.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.bruno.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @ControllerAdvice
@@ -20,5 +21,15 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandarError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+		
+		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violão de dados", ex.getMessage(),
+				request.getRequestURI());		
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
 
 }

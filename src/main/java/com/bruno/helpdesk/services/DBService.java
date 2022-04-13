d9d1632f4679286369686bc42567fdf055900174
BrunoN97 <nobre.piaget@gmail.com>
2022-04-13 20:21:38 -0300

    autenticacao com JWT

--- src/main/java/com/bruno/helpdesk/services/DBService.java
+++ src/main/java/com/bruno/helpdesk/services/DBService.java
@@ -3,6 +3,7 @@ package com.bruno.helpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bruno.helpdesk.domain.Chamado;
@@ -24,16 +25,18 @@ public class DBService {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 
 	public void instanciaDB() {
-		Tecnico tec1 = new Tecnico(null, "Bruno Nobre", "31267068868", "nobre.piaget@gmail.com", "123");
+		Tecnico tec1 = new Tecnico(null, "Bruno Nobre", "31267068868", "nobre.piaget@gmail.com", encoder.encode("123"));
 		tec1.addPerfil(Perfil.ADMIN);
-		Tecnico tec2 = new Tecnico(null, "Victor Nobre", "15552445805", "nobre@gmail.com", "123");
-		Tecnico tec3 = new Tecnico(null, "João Freire", "85812647860", "piaget@gmail.com", "123");
+		Tecnico tec2 = new Tecnico(null, "Victor Nobre", "15552445805", "nobre@gmail.com", encoder.encode("123"));
+		Tecnico tec3 = new Tecnico(null, "João Freire", "85812647860", "piaget@gmail.com", encoder.encode("123"));
 
-		Cliente cli1 = new Cliente(null, "Linus Torvades", "00007517823", "torvades@mail.com", "123");
-		Cliente cli2 = new Cliente(null, "Maria Torvades", "97718900851", "Maria@mail.com", "123");
-		Cliente cli3 = new Cliente(null, "Jorge Tonho", "36816313879", "Jorge@mail.com", "123");
+		Cliente cli1 = new Cliente(null, "Linus Torvades", "00007517823", "torvades@mail.com", encoder.encode("123"));
+		Cliente cli2 = new Cliente(null, "Maria Torvades", "97718900851", "Maria@mail.com", encoder.encode("123"));
+		Cliente cli3 = new Cliente(null, "Jorge Tonho", "36816313879", "Jorge@mail.com", encoder.encode("123"));
 
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1,
 				cli1);
